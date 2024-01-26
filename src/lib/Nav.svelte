d3099186901925482c4d4362e0ddf1016097ae3b
Vincent Kamemia <kamemia18@yahoo.com>
2024-01-26 16:46:23 +0300

    Initial commit

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -6,7 +6,7 @@
 	<div class="container flex flex-wrap justify-between items-center mx-auto">
 		<a href="/" class="flex items-center">
 			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
-				>Movie App</span
+				>TMDB Movie</span
 			></a
 		>
 
